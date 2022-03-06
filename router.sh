7a4fad47df7d7e96c27ff73ab7605844329179e1
jonnyVarn <71185833+jonnyVarn@users.noreply.github.com>
2022-03-06 22:12:22 +0100

    Update router.sh

--- router.sh
+++ router.sh
@@ -1,4 +1,4 @@
 #! /bin/ash
 echo "arouter" >/etc/hostname
-apk add iptables acf-iptables dnsmasq acf-dnsmasq openvpn afc-openvpn openrc
+apk add iptables acf-iptables dnsmasq acf-dnsmasq openvpn acf-openvpn openrc
 setup-acf
