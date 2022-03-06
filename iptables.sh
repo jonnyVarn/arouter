0d9b38c7f47cdcbec36ad62829aa3c16151f6aa5
jonnyVarn <71185833+jonnyVarn@users.noreply.github.com>
2022-03-06 21:19:37 +0100

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -6,7 +6,7 @@ rc-update add iptables
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 sysctl -p
 
-iptables -t nat -A POSTROUTING -o $WAN -j MASQUERADE
-iptables -A FORWARD -i $LAN -j ACCEPT
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth1 -j ACCEPT
 /etc/init.d/iptables save
 /etc/init.d/iptables restart
