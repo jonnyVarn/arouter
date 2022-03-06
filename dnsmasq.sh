6c1fea9e6f4868e0b4cc18ca8c876f994fdffd35
jonnyVarn <71185833+jonnyVarn@users.noreply.github.com>
2022-03-06 21:21:26 +0100

    Create dnsmasq.sh

---
+++ dnsmasq.sh
@@ -0,0 +1,12 @@
+#!/bin/ash
+
+apk add dnsmasq
+rc-update add dnsmasq
+
+echo "interface=eth1" >/etc/dnsmasq.conf 
+echo "except-interface=eth0" >> /etc/dnsmasq.conf
+echo "dhcp-range=subnet0,192.168.100.100,192.168.100.200,255.255.255.0,12h" >> /etc/dnsmasq.conf
+echo "bind-interfaces" >> /etc/dnsmasq.conf
+echo "log-queries" >> /etc/dnsmasq.conf
+echo "log-dhcp" >> /etc/dnsmasq.conf
+#dhcp-host=aa:bb:cc:dd,192.168.99.99,somehost
