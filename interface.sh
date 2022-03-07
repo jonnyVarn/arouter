93d029b97925a0db878c9337c671d58e311ceb16
jonnyVarn <71185833+jonnyVarn@users.noreply.github.com>
2022-03-07 17:54:59 +0100

    Update interface.sh

--- interface.sh
+++ interface.sh
@@ -1,11 +1,13 @@
 #! /bin/ash
-brctl addbr br0
+#brctl addbr br0
 echo "auto eth0" > /etc/network/interfaces 
 echo "iface eth0 inet dhcp" >> /etc/network/interfaces
 echo "auto eth1" >> /etc/network/interfaces
 echo "iface eth1 inet static" >> /etc/network/interfaces
-echo "auto br0 inet static" >> /etc/network/interfaces
-echo "    pre-up ifconfig eth1 up" >> /etc/network/interfaces
+echo "auto wlan0" >> /etc/network/interfaces
+echo "iface wlan0 inet static" >> /etc/network/interfaces
+echo "auto br0" >> /etc/network/interfaces
+echo "iface br0 inet static" >> /etc/network/interfaces
 echo "    bridge-ports eth1 wlan0" >> /etc/network/interfaces
 echo "    address 192.168.100.1" >> /etc/network/interfaces
 echo "    broadcaset 192.168.100.255" >> /etc/network/interfaces
