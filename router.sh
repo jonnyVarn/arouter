8e541952e5c1371d4c66a5c5331c2255ab3fd707
jonnyVarn <71185833+jonnyVarn@users.noreply.github.com>
2022-03-07 19:12:56 +0100

    Update router.sh

--- router.sh
+++ router.sh
@@ -1,4 +1,6 @@
 #! /bin/ash
 echo "arouter" >/etc/hostname
-apk add iptables acf-iptables dnsmasq acf-dnsmasq openvpn acf-openvpn openrc bridge
+apk add iptables acf-iptables dnsmasq acf-dnsmasq openvpn acf-openvpn openrc bridge linux-firmware-cypress
+rc-update add swclock boot    # enable the software clock
+rc-update del hwclock boot    # disable the hardware clock
 setup-acf
