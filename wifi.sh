b467846aff15d4460713fc2eb44f8288e31e6886
jonnyVarn <71185833+jonnyVarn@users.noreply.github.com>
2022-03-08 14:41:24 +0100

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -1,19 +1,19 @@
 #! /bin/sh
 apt install hostapd -y
-echo "interface=wlan0" >/etc/hostapd.conf
-echo "bridge=br0" >>/etc/hostapd.conf
-echo "hw_mode=n" >>/etc/hostapd.conf
-echo "channel=1" >>/etc/hostapd.conf
-echo "wmm_enabled=0" >>/etc/hostapd.conf
-echo "macaddr_acl=0" >>/etc/hostapd.conf
-echo "auth_algs=1" >>/etc/hostapd.conf
-echo "ignore_broadcast_ssid=0" >>/etc/hostapd.conf
-echo "wpa=2" >>/etc/hostapd.conf
-echo "wpa_key_mgmt=WPA-PSK" >>/etc/hostapd.conf
-echo "wpa_pairwise=TKIP" >>/etc/hostapd.conf
-echo "rsn_pairwise=CCMP" >>/etc/hostapd.conf
-echo "ssid=arouter" >>/etc/hostapd.conf
-echo "wpa_passphrase=password" >>/etc/hostapd.conf
+echo "interface=wlan0" >/etc/hostapd/hostapd.conf
+echo "bridge=br0" >>/etc/hostapd/hostapd.conf
+echo "hw_mode=g" >>/etc/hostapd/hostapd.conf
+echo "channel=1" >>/etc/hostapd/hostapd.conf
+echo "wmm_enabled=0" >>/etc/hostapd/hostapd.conf
+echo "macaddr_acl=0" >>/etc/hostapd/hostapd.conf
+echo "auth_algs=1" >>/etc/hostapd/hostapd.conf
+echo "ignore_broadcast_ssid=0" >>/etc/hostapd/hostapd.conf
+echo "wpa=2" >>/etc/hostapd/hostapd.conf
+echo "wpa_key_mgmt=WPA-PSK" >>/etc/hostapd/hostapd.conf
+echo "wpa_pairwise=TKIP" >>/etc/hostapd/hostapd.conf
+echo "rsn_pairwise=CCMP" >>/etc/hostapd/hostapd.conf
+echo "ssid=arouter" >>/etc/hostapd/hostapd.conf
+echo "wpa_passphrase=password" >>/etc/hostapd/hostapd.conf
 
 
 
